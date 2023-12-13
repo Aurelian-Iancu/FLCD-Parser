f1850a0ea4d3dc60460424351499e7c5a7ab01ff
iamnotfelix <andrei.iliescu03@gmail.com>
2023-12-13 19:10:22 +0200

    Added missing files.

---
+++ Grammar/Grammar/HashTable.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <vector>
+#include <list>
+#include <cmath>
+#include <string>
+#include <utility>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+template <class T> 
+class HashTable
+{
+private:
+    int codeIndex;
+    const int PRIME_CONST = 131;
+
+    struct Node {
+        T value;
+        int code;
+    };
+
+    int hashFunction(string key) {
+        return (key.length() * PRIME_CONST) % this->size;
+       /* unsigned long hash = 0;
+        for (int i = 0; i < key.length(); i++)
+        {
+            hash = (hash * PRIME_CONST) + key[i];
+        }
+        return hash % this->size;*/
+    }
+
+public:
+    int size;
+    vector<list<Node>> table;
+
+    HashTable();
+    HashTable(int size);
+    int insert(T value);
+    pair<bool, int> search(T key);
+    void displayHash();
+    void dumpToFile();
+
+    HashTable& operator=(const HashTable& table);
+};
+
+
+template<class T>
+HashTable<T>::HashTable() {
+    this->size = 0;
+    this->codeIndex = 0;
+    this->table = vector<list<Node>>(size);
+}
+
+template<class T>
+HashTable<T>::HashTable(int size) {
+    this->size = size;
+    this->codeIndex = 1;
+    this->table = vector<list<Node>>(size);
+}
+
+template<class T>
+HashTable<T>& HashTable<T>::operator=(const HashTable<T>& table) {
+    this->size = table.size;
+    this->codeIndex = table.codeIndex;
+    this->table = table.table;
+
+    return *this;
+}
+
+template<class T>
+int HashTable<T>::insert(T value) {
+    auto search = this->search(value);
+    if (search.first) return search.second;
+ 
+    stringstream ss;
+    ss << value;
+    string key = ss.str();
+    int hash = this->hashFunction(key);
+
+    Node tmp;
+    tmp.code = this->codeIndex++;
+    tmp.value = value;
+
+    this->table[hash].push_back(tmp);
+
+    return tmp.code;
+}
+
+template<class T>
+pair<bool, int> HashTable<T>::search(T key) {
+    stringstream ss;
+    ss << key;
+    string k = ss.str();
+    int hash = this->hashFunction(k);
+
+    if (this->table[hash].empty()) return { false, 0 };
+
+    for (auto node : this->table[hash]) {
+        if (node.value == key) return { true, node.code };
+    }
+
+    return { false, 0 };
+}
+
+template<class T>
+void HashTable<T>::displayHash() {
+    for (auto l : this->table) {
+        for (auto node : l) {
+            cout << node.value << " " << node.code << endl;
+        }
+    }
+}
+
+template<class T>
+void HashTable<T>::dumpToFile() {
+    ofstream g("st.out");
+    for (auto l : this->table) {
+        for (auto node : l) {
+            g << node.value << " " << node.code << endl;
+        }
+    }
+}
---
+++ Grammar/Grammar/main.cpp
@@ -0,0 +1,94 @@
+#include "HashTable.h"
+#include "Scanner.h"
+#include "FiniteAutomata.h"
+#include "Menu.h"
+#include "Grammar.h"
+#include "Parser.h"
+
+#include <iostream>
+
+void testScanner()
+{
+	FiniteAutomata faIntConst{ "C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week6\\fa_int_const.in" };
+	FiniteAutomata faIdentifier{ "C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week6\\fa_identifier.in" };
+
+	Scanner scanner(
+		"C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week2\\token.in",
+		"C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week2\\separator.in",
+		"C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week2\\operator.in",
+		"C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week2\\reservedWord.in",
+		faIntConst,
+		faIdentifier
+	);
+	try
+	{
+		scanner.scan("C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week1\\p1.ili");
+		//scanner.displayPif();
+		scanner.dumpToFile();
+	}
+	catch (runtime_error e)
+	{
+		cout << e.what();
+	}
+}
+
+void testFA()
+{
+	FiniteAutomata faIntConst{ "C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week6\\fa_int_const.in" };
+	cout << faIntConst.check("12") << endl;
+	cout << faIntConst.check("-12") << endl;
+	cout << faIntConst.check("++12") << endl;
+	cout << faIntConst.check("0+12") << endl;
+	cout << faIntConst.check("-0") << endl;
+	cout << faIntConst.check("-a") << endl;
+	cout << faIntConst.check("aasdf") << endl;
+	cout << faIntConst.check("+aasdf") << endl;
+	cout << faIntConst.check("0") << endl;
+	cout << faIntConst.check("1231231231") << endl;
+	//fa.displayFA();
+
+	FiniteAutomata faIdentifier{ "C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week6\\fa_identifier.in" };
+	cout << faIdentifier.check("___") << endl;
+	cout << faIdentifier.check("12") << endl;
+	cout << faIdentifier.check("_asdf3_") << endl;
+	cout << faIdentifier.check("__aAdf3_") << endl;
+	cout << faIdentifier.check("_333__") << endl;
+	cout << faIdentifier.check("__a_") << endl;
+	cout << faIdentifier.check("adf4___") << endl;
+	cout << faIdentifier.check("ABFdf4_S__") << endl;
+	cout << faIdentifier.check("4___") << endl;
+	//faIdentifier.displayFA();
+
+	Menu menu(faIntConst);
+	menu.start();
+}
+
+void testGrammar() 
+{
+	Grammar g{ "C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week8\\g2.txt" };
+
+	g.displayNonTerminals();
+	g.displayTerminals();
+	g.displayProduction("constant");
+}
+
+void testParser()
+{
+	Grammar g{ "C:\\Users\\andre\\Desktop\\folders\\facultate\\sem_5\\compilers\\week8\\g3.txt" };
+	Parser p(g);
+
+	//g.displayProductions();
+
+	p.createCanonicalCollection();
+	cout << p.toString();
+}
+
+int main() 
+{
+	//testScanner();
+	//testFA();
+	//testGrammar();
+	testParser();
+
+	return 0;
+}
